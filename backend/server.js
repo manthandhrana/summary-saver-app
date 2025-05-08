f6a8840914e75b15fef4ce73aa1ab06e01b044f1
Manthan Dhrana <dhranamanthan201318@gmail.com>
2025-05-08 12:01:29 +0530

    backend start app

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/bookmarks", require("./routes/bookmarkRoutes"));
+app.get("/", (res,res)=>{
+  res.statusCode(200).send("Welcome to Summary Saver App")
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
